517ee0dfcca0189e210b4f10476dd7efd9a137c5
cocodding0723 <46515671+cocodding0723@users.noreply.github.com>
2021-09-20 12:44:31 +0900

    Add SlowMotion

---
+++ Assets/Dev_Chanhyeong/2_Scripts/SlowMotionEffect.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class SlowMotionEffect : MonoBehaviour {
+    private Volume volume;
+
+    [SerializeField]
+    private float modifier = 5f;
+    [SerializeField]
+    private float slowMotion_timeScale = 0.25f;
+
+    private void Start() {
+        volume = this.GetComponent<Volume>();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(1)){
+            volume.weight = Mathf.Lerp(volume.weight, 1f, modifier * Time.deltaTime);
+            Time.timeScale = Mathf.Lerp(Time.timeScale, slowMotion_timeScale, modifier * Time.deltaTime);
+        }
+        else{
+            volume.weight = Mathf.Lerp(volume.weight, 0f, modifier * Time.deltaTime);
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 1f, modifier * Time.deltaTime);
+        }
+    }
+}
